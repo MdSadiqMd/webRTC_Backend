bd1f89d4be5b5aa180ce5b57c4c7ec5d00167f4c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-09-09 17:00:48 +0530

    Refactor: Removed Hard cord sockets

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -4,4 +4,10 @@ dotenv.config();
 
 export default {
     PORT: process.env.PORT || 3000,
+    ROOM_SOCKET: 'room-created',
+    USER_JOINED_SOCKET: 'user-joined',
+    READY_SOCKET: 'ready',
+    USERS_SOCKET: 'get-users',
+    JOINED_SOCKET: 'joined-room',
+    CREATE_SOCKET: 'create-room',
 };
--- src/handlers/room.handler.ts
+++ src/handlers/room.handler.ts
@@ -1,7 +1,7 @@
 import { Socket } from 'socket.io';
 import { v4 as UUIDv4 } from 'uuid';
 
-import { logger } from '../config';
+import { logger, serverConfig } from '../config';
 import { IRoomParams } from '../types/IRoomParams.types';
 
 const rooms: Record<string, string[]> = {};
@@ -12,7 +12,7 @@ const roomHandler = (socket: Socket) => {
         socket.join(roomId);
         rooms[roomId] = [];
 
-        socket.emit('room-created', { roomId });
+        socket.emit(serverConfig.ROOM_SOCKET, { roomId });
         logger.info(`Room Created withId: ${roomId}`);
     };
 
@@ -23,19 +23,19 @@ const roomHandler = (socket: Socket) => {
 
             logger.info(`A New User with userId: ${peerId} Joined with SocketId: ${socket.id} in Room: ${roomId}`);
 
-            socket.on('ready', () => {
-                socket.to(roomId).emit('user-joined', { peerId });
+            socket.on(serverConfig.READY_SOCKET, () => {
+                socket.to(roomId).emit(serverConfig.USER_JOINED_SOCKET, { peerId });
             });
-            
-            socket.emit('get-users', {
+
+            socket.emit(serverConfig.USERS_SOCKET, {
                 roomId,
                 participants: rooms[roomId]
             });
         }
     };
 
-    socket.on('create-room', createRoom);
-    socket.on('joined-room', joinedRoom);
+    socket.on(serverConfig.CREATE_SOCKET, createRoom);
+    socket.on(serverConfig.JOINED_SOCKET, joinedRoom);
 };
 
 export default roomHandler;
